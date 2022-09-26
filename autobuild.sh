48ffc4e078747cfb864916148d54045d44ea1e42
0xKumi <dung.q@autonomous.nyc>
2022-09-27 00:07:00 +0700

    update build script

---
+++ autobuild.sh
@@ -0,0 +1,31 @@
+cat <<EOF > Dockerfile2
+FROM golang:alpine
+# Install minimum necessary dependencies,
+ENV PACKAGES curl make git libc-dev bash gcc linux-headers eudev-dev python3
+RUN apk add --no-cache \$PACKAGES
+# Set working directory for the build
+WORKDIR /go/src/github.com/cosmos/cosmos-sdk
+RUN git config --global --add safe.directory /go/src/github.com/cosmos/cosmos-sdk
+EOF
+docker build -t cosmos-build -f Dockerfile2 .
+rm Dockerfile2
+
+docker run -ti --rm -v $GOPATH/build_mod:/go/pkg -v $GOPATH/build_cache:/root/.cache -v $PWD:/go/src/github.com/cosmos/cosmos-sdk --name cosmos-build cosmos-build make build-linux
+
+cp build/simd ./simd
+cat <<EOF >Dockerfile3
+FROM alpine:edge
+# Install ca-certificates
+RUN apk add --update ca-certificates
+WORKDIR /root
+# Copy over binaries from the build-env
+COPY simd /usr/bin/simd
+COPY scripts/init-chain.sh  ./init-chain.sh
+EXPOSE 26656 26657 1317 9090
+# Run simd by default, omit entrypoint to ease using container with simcli
+CMD ["simd"]
+EOF
+docker build -t simapp -f Dockerfile3 .
+rm simd Dockerfile3
+
+docker run -ti --rm -v /tmp/testchain:/root/.simapp --name simapp simapp sh init-chain.sh
--- scripts/init-chain.sh
+++ scripts/init-chain.sh
@@ -1,7 +1,18 @@
-simd keys add my_validator
-MY_VALIDATOR_ADDRESS=$(simd keys show my_validator -a)
-simd init name --chain-id my-test-chain
-simd add-genesis-account  $MY_VALIDATOR_ADDRESS 100000000000stake
-simd gentx my_validator 100000000stake --chain-id my-test-chain
+if [ ! -f /root/.simapp/my_validator.info ]; then
+  echo "Create my_validator key"
+  echo -e "12345678\n12345678" | simd keys add my_validator
+fi
+
+MY_VALIDATOR_ADDRESS=$(echo 12345678 | simd keys show my_validator -a )
+echo $MY_VALIDATOR_ADDRESS
+
+if [ ! -f /root/.simapp/config/genesis.json ]; then
+  simd init name --chain-id my-test-chain
+else
+  echo "Chain already init genesis"
+fi
+
+simd add-genesis-account $MY_VALIDATOR_ADDRESS 100000000000stake
+echo 12345678 |  simd gentx my_validator 100000000stake --chain-id my-test-chain
 simd collect-gentxs
 simd start

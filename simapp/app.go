f8a867b0a46b284691bfe402a65daaa9c4b4b2bb
0xKumi <dung.q@autonomous.nyc>
2022-10-05 04:34:35 +0700

    update privacy module account

--- simapp/app.go
+++ simapp/app.go
@@ -131,6 +131,7 @@ var (
 		stakingtypes.BondedPoolName:    {authtypes.Burner, authtypes.Staking},
 		stakingtypes.NotBondedPoolName: {authtypes.Burner, authtypes.Staking},
 		govtypes.ModuleName:            {authtypes.Burner},
+		privacyTypes.ModuleName:        nil,
 	}
 )
 
